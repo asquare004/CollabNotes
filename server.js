b89b78000d642948a7907717aca972968ea1435e
unknown <andersarnold123@gmail.com>
2025-01-23 13:14:13 +0530

    Added shared chatting feature

--- server.js
+++ server.js
@@ -1,79 +1,50 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
-// Global variables to hold all usernames and rooms created
-var usernames = {};
-var rooms = [
-  { name: "global", creator: "Anonymous" },
-  { name: "chess", creator: "Anonymous" },
-];
-
-io.on("connection", function (socket) {
-  console.log(`User connected to server.`);
-
-  socket.on("createUser", function (username) {
-    socket.username = username;
-    usernames[username] = username;
-    socket.currentRoom = "global";
-    socket.join("global");
-
-    console.log(`User ${username} created on server successfully.`);
-
-    socket.emit("updateChat", "INFO", "You have joined global room");
-    socket.broadcast
-      .to("global")
-      .emit("updateChat", "INFO", username + " has joined global room");
-    io.sockets.emit("updateUsers", usernames);
-    socket.emit("updateRooms", rooms, "global");
-  });
-
-  socket.on("sendMessage", function (data) {
-    io.sockets.to(socket.currentRoom).emit("updateChat", socket.username, data);
-  });
-
-  socket.on("createRoom", function (room) {
-    if (room != null) {
-      rooms.push({ name: room, creator: socket.username });
-      io.sockets.emit("updateRooms", rooms, null);
-    }
-  });
-
-  socket.on("updateRooms", function (room) {
-    socket.broadcast
-      .to(socket.currentRoom)
-      .emit("updateChat", "INFO", socket.username + " left room");
-    socket.leave(socket.currentRoom);
-    socket.currentRoom = room;
-    socket.join(room);
-    socket.emit("updateChat", "INFO", "You have joined " + room + " room");
-    socket.broadcast
-      .to(room)
-      .emit(
-        "updateChat",
-        "INFO",
-        socket.username + " has joined " + room + " room"
-      );
-  });
-
-  socket.on("disconnect", function () {
-    console.log(`User ${socket.username} disconnected from server.`);
-    delete usernames[socket.username];
-    io.sockets.emit("updateUsers", usernames);
-    socket.broadcast.emit(
-      "updateChat",
-      "INFO",
-      socket.username + " has disconnected"
-    );
-  });
+// Serve the frontend.html file at the root route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "frontend.html"));
 });
 
-server.listen(5500, function () {
-  console.log("Listening to port 5500.");
+// Socket.io logic
+let users = {};
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    // Register new user
+    socket.on("createUser", (username) => {
+        users[socket.id] = username;
+        socket.broadcast.emit("updateChat", "INFO", `${username} joined the chat`);
+        socket.emit("updateChat", "INFO", "Welcome to the chat!");
+    });
+
+    // Handle sending messages
+    socket.on("sendMessage", (message) => {
+        const username = users[socket.id] || "Anonymous";
+        io.emit("updateChat", username, message);
+    });
+
+    // Handle user disconnect
+    socket.on("disconnect", () => {
+        const username = users[socket.id];
+        delete users[socket.id];
+        if (username) {
+            io.emit("updateChat", "INFO", `${username} left the chat`);
+        }
+    });
 });
+
+// Start the server
+const PORT = process.env.PORT || 5500;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
